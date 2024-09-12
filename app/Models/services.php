d352e4d2b879e282f689ba5ba90767b3f9632faa
guillermogm <guillermogm009@gmail.com>
2024-09-12 09:19:25 +0200

    feat: service model

--- app/Models/services.php
+++ app/Models/services.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\appointments;
 
 class services extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function appointments()
+    {
+        return $this->hasMany(appointments::class, 'service_id');
+    }
 }
