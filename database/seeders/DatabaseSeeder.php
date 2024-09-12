75d9f7292bcf06eef3579778d862f6a77aa79d87
guillermogm <guillermogm009@gmail.com>
2024-09-12 10:03:05 +0200

    feat: User seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\UserSeeder;
+use Illuminate\Database\UserSeeders;
 
 class DatabaseSeeder extends Seeder
 {
